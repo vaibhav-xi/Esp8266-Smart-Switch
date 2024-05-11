eeb7d6f759b803b1f9c83ef458129f5636148ddd
Vaibhav <vaibhav28890@gmail.com>
2024-05-12 00:50:56 +0530

    ota enabled, rgb error status

--- SmartSwitch/SmartSwitch.ino
+++ SmartSwitch/SmartSwitch.ino
@@ -1,5 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 // Replace these variables with your Wi-Fi credentials
 const char* ssid = "wifi_ssid";
@@ -9,6 +12,11 @@ ESP8266WebServer server(80);
 
 // Pin connected to Relay
 const int relayPin = D3;  // Change this to the pin you want to control
+
+// Pins connected to RGB LED (common cathode)
+const int redPin = D4;
+const int greenPin = D5;
+const int bluePin = D6;
 
 void setup() {
   // Initialize Serial Monitor
@@ -32,6 +40,15 @@ void setup() {
   pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, LOW);
 
+  // Initialize RGB LED pins
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  // Turn off RGB LED
+  digitalWrite(redPin, LOW);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(bluePin, LOW);
+
   // Route for root / web page
   server.on("/", HTTP_GET, []() {
     // HTML for the web page with on/off button
@@ -53,8 +70,54 @@ void setup() {
 
   server.begin();
   Serial.println("HTTP server started");
+
+  // Initialize OTA
+  ArduinoOTA.onStart([]() {
+    Serial.println("OTA Update Start");
+    // Blink red LED to indicate OTA update start
+    setColor(255, 0, 0); // Red
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("OTA Update End");
+    // Turn off all LEDs at OTA update end
+    setColor(0, 0, 0); // Turn off
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("OTA Progress: %u%%\r", (progress / (total / 100)));
+    // Blink blue LED to indicate OTA update progress
+    setColor(0, 0, 255); // Blue
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("OTA Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+      setColor(255, 0, 255); // Magenta
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+      setColor(255, 255, 0); // Yellow
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+      setColor(255, 165, 0); // Orange
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+      setColor(255, 140, 0); // Dark Orange
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+      setColor(255, 69, 0); // Red-Orange
+    }
+  });
+
+  ArduinoOTA.begin();
 }
 
 void loop() {
   server.handleClient();
+  ArduinoOTA.handle();
+}
+
+// Function to set RGB LED color
+void setColor(int red, int green, int blue) {
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);
 }
